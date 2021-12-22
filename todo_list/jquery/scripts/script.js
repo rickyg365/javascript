cba98ae9fab251cb06e07a37c761a468c008ab22
rickyg365 <rickygonzal3z@hotmail.com>
2021-12-22 14:24:30 -0800

    jquery

---
+++ todo_list/jquery/scripts/script.js
@@ -0,0 +1,10 @@
+// Wait for document to be ready
+$(document).ready(function(){
+
+    // jQuery methods go here...
+    let addTask = (parent_element_selector) => {
+        let new_task = $(parent_element_selector).append();
+    };
+
+  
+});
