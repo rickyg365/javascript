c3412d6d2d327d9ab41685273475c020e2c5c92e
rickyg365 <rickygonzal3z@hotmail.com>
2022-01-27 10:10:37 -0800

    updated codebase

--- react/dash/src/App.js
+++ react/dash/src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
+import constructionLogo from "./construction_logo_rect.svg";
 // import NavItem from "./components/Item";
 import NavBar from "./components/Navbar.js";
 
 function App() {
     return (
         <div className="App">
+            <div className="banner">
+                <img src={constructionLogo} alt="Under Construction Logo" />
+                <h1 className="banner-txt">Check back Soon!</h1>
+            </div>
             {/* <NavItem /> */}
             <NavBar></NavBar>
             <main>
-                <h1>Main Body</h1>
+                <section>
+                    {/* <h1>Main Body</h1> */}
+                    <div className="display">
+                        <h1>No Display</h1>
+                        {/* <img
+                            src={constructionLogo}
+                            alt="Under Construction Logo"
+                        ></img> */}
+                    </div>
+                </section>
             </main>
         </div>
     );
