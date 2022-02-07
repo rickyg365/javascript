98ec312229bd4d7b5c5890950c504425e1e2c353
rickyg365 <rickygonzal3z@hotmail.com>
2022-02-07 11:15:00 -0800

    base code for react_resume, missing Experience, Projects and Skills sections

--- react/resume/src/index.js
+++ react/resume/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Resume from "./Resume";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Resume />, document.getElementById("root"));
